cb80970948a8397afc6142ea0c501f0421190a53
Alice Shao <a1ice_shao@yahoo.com>
2022-10-25 20:01:20 -0400

    restructured questions lists

---
+++ generateHTML.js
@@ -0,0 +1,52 @@
+function TeamManager(name, id, email, number){
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.number = number;
+
+    this.managerCard = (manager) => {
+        return `<div class="card" style="width: 18rem;">
+        <div class="card-header">
+            ${manager.name}
+            Team Manager
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">${manager.id}</li>
+            <li class="list-group-item">${manager.email}</li>
+            <li class="list-group-item">${manager.number}</li>
+        </ul>
+    </div>`
+    }
+}
+
+function generateHTML(data) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Team Information</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+            integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    </head>
+    
+    <body>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">My Team</h1>
+                <p class="lead">Welcome to my team page. Here you can find their basic contact information.</p>
+            </div>
+        </div>
+    
+        ${new TeamManager(data)}
+    
+    </body>
+    
+    </html>
+    `
+}
+
+
+module.exports = generateHTML;
--- index.js
+++ index.js
@@ -1,41 +1,123 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateHTML = require("./generateHTML");
 
-const questions =[
-{
-    type: "input",
-    message: "Please enter the team manager's name: ",
-    name: "teamManagerName"
-},
-{
-    type: "input",
-    message: "Please enter the team manager's employee ID: ",
-    name: "teamManagerID"
-},
-{
-    type: "input",
-    message: "Please enter the team manager's email: ",
-    name: "teamManagerEmail"
-},
-{
-    type: "input",
-    message: "Please enter the team manager's office number: ",
-    name: "teamManagerNum"
-},
-{
-    type: "choice",
-    message: "Do you have more employees to add?",
-    choices: ["Engineer", "Intern", "No, I'm done"],
-    name: "otherEmployees"
-},
-{
-    type: "input",
-    message: "Please enter the engineer's name: ",
-    name: "engineer-name",
-    // when user selects engineer
-    when: (choice) => choice.otherEmployees[0] === "Engineer"
-},
-{
-    
-}
+
+// Manager questions
+const questions = [
+    {
+        type: "input",
+        message: "Please enter the team manager's name: ",
+        name: "teamManagerName"
+    },
+    {
+        type: "input",
+        message: "Please enter the team manager's employee ID: ",
+        name: "teamManagerID"
+    },
+    {
+        type: "input",
+        message: "Please enter the team manager's email: ",
+        name: "teamManagerEmail"
+    },
+    {
+        type: "input",
+        message: "Please enter the team manager's office number: ",
+        name: "teamManagerNum"
+    }
 ]
+
+// Engineer questions
+const questions2 = [
+    {
+        type: "checkbox",
+        message: "Do you have more employees to add?",
+        choices: ["Engineer", "Intern", "No, I'm done"],
+        name: "otherEmployees"
+    },
+    {
+        type: "input",
+        message: "Please enter the engineer's name: ",
+        name: "engineerName",
+        when: (choice) => choice.otherEmployees[0] === "Engineer"
+    },
+    {
+        type: "input",
+        message: "Please enter the engineer's employee ID: ",
+        name: "engineerID",
+        when: (choice) => choice.otherEmployees[0] === "Engineer"
+    },
+    {
+        type: "input",
+        message: "Please enter the engineer's email: ",
+        name: "engineerEmail",
+        when: (choice) => choice.otherEmployees[0] === "Engineer"
+    },
+    {
+        type: "input",
+        message: "Please enter the engineer's office number: ",
+        name: "engineerNum",
+        when: (choice) => choice.otherEmployees[0] === "Engineer"
+    },
+    {
+        type: "input",
+        message: "Please enter the intern's name: ",
+        name: "internName",
+        when: (choice) => choice.otherEmployees[0] === "Intern"
+    },
+    {
+        type: "input",
+        message: "Please enter the engineer's employee ID: ",
+        name: "internID",
+        when: (choice) => choice.otherEmployees[0] === "Intern"
+    },
+    {
+        type: "checkbox",
+        message: "Do you have more employees to add?",
+        choices: ["Intern", "No, I'm done"],
+        name: "otherEmployees2"
+    }
+]
+
+// Intern questions
+const questions3 = [
+    {
+        type: "input",
+        message: "Please enter the intern's name: ",
+        name: "internName",
+    },
+    {
+        type: "input",
+        message: "Please enter the engineer's employee ID: ",
+        name: "internID"
+    },
+    {
+        type: "input",
+        message: "Please enter the engineer's email: ",
+        name: "internEmail"
+    },
+    {
+        type: "input",
+        message: "Please enter the engineer's office number: ",
+        name: "internNum"
+    },
+]
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log("Success!"))
+}
+
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        console.log(answers);
+        writeToFile(("./dist/test.html"), generateHTML(answers));
+    })
+    .then((answers2) => {
+        console.log(answers2);
+        writeToFile(("./dist/test.html"), generateHTML(answers2));
+    })
+}
+
+init()
